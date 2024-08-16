d3cc873f3c05f3c5b797964502c0542509d4af5c
SM Khobaib Alam <sm.khobaib.alam@walmart.com>
2024-08-16 11:56:21 +0530

    file refactor

--- snake-ladder/src/main.js
+++ snake-ladder/src/main.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
+const path = require('path');
 const { readInput, isGameOver, getWinningPos, announceWinner, nextMove } = require('./utils/index.js');
 
-const input = fs.readFileSync('resource/input.txt', { encoding: 'utf-8'});
+const inputFilePath = path.resolve(__dirname, '../resources/input.txt');
+const input = fs.readFileSync(inputFilePath, { encoding: 'utf-8'});
 const { snakes, ladders, players: { names, positions } } = readInput(input);
 const numPlayers = positions.length;
 
